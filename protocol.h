8f95b7f8e6bc02f695c84e8bf4ce985340683b7e
Patrik Arlos <patrik.arlos@gmail.com>
2025-09-09 10:58:01 +0200

    Initial commit

---
+++ protocol.h
@@ -0,0 +1,58 @@
+#include <stdint.h>
+
+/* 
+   Used in both directions; if 
+   server->client,type should be set to 1, 
+   client->server type = 2. 
+ */
+struct  __attribute__((__packed__)) calcProtocol{
+  uint16_t type;  // What message is this, 1 = server to client, 2 client to server, 3... reserved , conversion needed 
+  uint16_t major_version; // 1, conversion needed 
+  uint16_t minor_version; // 1, conversion needed 
+  uint32_t id; // Server side identification with operation. Client must return the same ID as it got from Server., conversion needed 
+  uint32_t arith; // What operation to perform, see mapping below. 
+  int32_t inValue1; // integer value 1, conversion needed 
+  int32_t inValue2; // integer value 2, conversion needed 
+  int32_t inResult; // integer result, conversion needed 
+};
+
+  
+struct  __attribute__((__packed__)) calcMessage {
+  uint16_t type;    // See below, conversion needed 
+  uint32_t message; // See below, conversion needed 
+  
+  // Protocol, UDP = 17, TCP = 6, other values are reserved. (UDP+TCP = 17|6)
+  uint16_t protocol; // conversion needed 
+  uint16_t major_version; // 1, conversion needed 
+  uint16_t minor_version; // 1 , conversion needed 
+
+};
+
+
+/* arith mapping in calcProtocol
+1 - add
+2 - sub
+3 - mul
+4 - div
+
+other numbers are reserved
+
+*/
+
+
+/* 
+   calcMessage.type
+   1 - server-to-client, text protocol
+   2 - server-to-client, binary protocol
+   3 - server-to-client, binary+text;
+   21 - client-to-server, text protocol
+   22 - client-to-server, binary protocol
+   23 - client-to-serve, N/A
+   
+   calcMessage.message 
+
+   0 = Not applicable/availible (N/A or NA)
+   1 = OK   // Accept 
+   2 = NOT OK  // Reject 
+
+*/
